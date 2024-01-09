56bac0a51aedaac0a726d70ca77b0d56b3d742e5
Mr. Anderson <mpanderson06@fastmail.com>
2024-01-09 16:41:34 -0600

    Adding better error logging. App only says online if everything is successful (#26)

--- src/helpers/mqtt_connect.js
+++ src/helpers/mqtt_connect.js
@@ -6,13 +6,18 @@ const onMqttConnect = () => {
 	const opt = { qos: 2, retain: true };
 	g.Globals.mqttClient.subscribe(g.Globals.invConsumeTopic, opt, function (err) {
 		if (err) {
-			log.error('Error subscribing to inventory consume topic', err)
+			const msg = `Error subscribing to inventory consume topic ${g.Globals.invConsumeTopic}`
+			log.error(msg, err);
+			g.exitAppEarly(msg);
 		}
 	});
 
 	g.Globals.mqttClient.subscribe(g.Globals.addUpdateItemTopic, opt, function (err) {
 		if (err) {
-			log.error('Error subscribing to add update topic', err);
+			const msg = `Error subscribing to add update topic ${g.Globals.addUpdateItemTopic}`
+			log.error(msg, err);
+			g.exitAppEarly(msg);
+			return;
 		}
 	});
 
