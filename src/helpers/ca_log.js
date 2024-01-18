dce2170a483eb31d67ba464f1644424263cc3bcc
Matt Anderson <mpanderson06@fastmail.com>
2024-01-18 15:24:58 -0600

    Simplifying code.  Adding another log level.  Conditionally logging based on level

--- src/helpers/ca_log.js
+++ src/helpers/ca_log.js
@@ -3,7 +3,7 @@ const m = require('./mqtt_publish');
 
 const error = async (message, err, data) => {
 	try {
-		await sendData(createLogObject(message, err, 'Error', data));
+		await logHelper(message, err, data, 'Error');
 	} catch (err) {
 		console.log('Error in ca_log.error', err);
 	}
@@ -11,10 +11,36 @@ const error = async (message, err, data) => {
 
 const verbose = async (message, data) => {
 	try {
-		await sendData(createLogObject(message, null, 'Verbose', data));
+		await logHelper(message, null, data, 'Verbose');
 	} catch (err) {
 		console.log('Error in ca_log.verbose', err);
 	}
+}
+
+const debug = async (message, data) => {
+	try {
+		await logHelper(message, null, data, 'Debug');
+	} catch (err) {
+		console.log('Error in ca_log.verbose', err);
+	}
+}
+
+const logHelper = async (message, err, data, level) => {
+	if (!shouldLog(level))
+		return;
+	await sendData(createLogObject(message, err, level, data));
+}
+
+function shouldLog(level) {
+	if (g.Globals.logLevel.includes('*'))
+		return true;
+	if (g.Globals.logLevel.includes(level))
+		return true;
+	if (level === 'Error')
+		return true;
+	if (level === 'Debug' && g.Globals.logLevel.includes('Verbose'))
+		return true; // Debug should also log verbose logs, even if the setup didn't specifically call for it
+	return false;
 }
 
 function createLogObject(message, err, logType, data) {
@@ -27,8 +53,16 @@ function createLogObject(message, err, logType, data) {
 		LogType: logType,
 		Data: JSON.stringify(data)
 	};
-	let objString = JSON.stringify(obj, null, 2);
+	let objString = JSON.stringify(obj, jsonReplacer, 2);
 	return objString;
+}
+
+function jsonReplacer(key, value) {
+	// Filtering out properties
+	if (value === null) {
+		return undefined;
+	}
+	return value;
 }
 
 async function sendData(objString) {
@@ -42,6 +76,7 @@ function logToConsole(message) {
 }
 
 module.exports = {
+	debug,
 	verbose,
 	error
 };
